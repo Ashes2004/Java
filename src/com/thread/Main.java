73e3739f7c22b5712406359da75fe617e456bd97
Ashes Das <dasashes04@gmail.com>
2025-09-13 09:46:13 +0530

    thread starting

---
+++ src/com/thread/Main.java
@@ -0,0 +1,9 @@
+package com.thread;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        thread1 t1 = new thread1();
+        t1.start();
+    }
+}
---
+++ src/com/thread/thread1.java
@@ -0,0 +1,10 @@
+package com.thread;
+
+public class thread1 extends Thread {
+   public void run()
+   {
+       System.out.println("this is thread");
+
+   }
+
+}
